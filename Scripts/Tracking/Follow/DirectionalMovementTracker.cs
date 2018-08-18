9f519a559164407246e041a58d3dcc7219690ce2
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-08-18 12:49:40 +0200

    feat(Follow): track movement along a direction vector
    A new component has been added to allow checking whether a Game
    Object is moving along a given direction. Events are emitting
    whenever the movement along the direction starts or stops.

---
+++ Scripts/Tracking/Follow/DirectionalMovementTracker.cs
@@ -0,0 +1,99 @@
+﻿namespace VRTK.Core.Tracking.Follow
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using VRTK.Core.Data.Source;
+    using VRTK.Core.Process;
+
+    /// <summary>
+    /// Tracks movement of a <see cref="GameObject"/> along a direction.
+    /// </summary>
+    public class DirectionalMovementTracker : MonoBehaviour, IProcessable
+    {
+        /// <summary>
+        /// The source of the movement to track.
+        /// </summary>
+        [Tooltip("The source of the movement to track.")]
+        public GameObject source;
+        /// <summary>
+        /// Defines the direction to check the movement against.
+        /// </summary>
+        [Tooltip("Defines the direction to check the movement against.")]
+        public Vector3SourceContainer directionSource;
+        /// <summary>
+        /// A threshold in the range of 0f to 1f to apply when checking for movement along the direction. 1f means the movement has to match the direction exactly, 0f accepts any movement.
+        /// </summary>
+        [Tooltip("A threshold in the range of 0f to 1f to apply when checking for movement along the direction. 1f means the movement has to match the direction exactly, 0f accepts any movement.")]
+        public float directionalSimilarityThreshold = 0.1f;
+
+        /// <summary>
+        /// Emitted when <see cref="source"/> starts moving along the direction of <see cref="directionSource"/>.
+        /// </summary>
+        public UnityEvent StartedMovingAlongDirection = new UnityEvent();
+        /// <summary>
+        /// Emitted when <see cref="source"/> stops moving along the direction of <see cref="directionSource"/>.
+        /// </summary>
+        public UnityEvent StoppedMovingAlongDirection = new UnityEvent();
+
+        public bool IsMovingAlongDirection
+        {
+            get;
+            protected set;
+        }
+
+        protected Vector3? previousPosition;
+
+        /// <summary>
+        /// Checks for movement along the direction and emits events when a change occurs.
+        /// </summary>
+        public virtual void Process()
+        {
+            if (CheckForMovementAlongDirection())
+            {
+                EmitMovementAlongDirectionEvent();
+            }
+
+            previousPosition = source.transform.position;
+        }
+
+        /// <summary>
+        /// Checks for movement along the direction.
+        /// </summary>
+        /// <returns>Whether the state of moving along the direction changed.</returns>
+        protected virtual bool CheckForMovementAlongDirection()
+        {
+            if (previousPosition == null)
+            {
+                return false;
+            }
+
+            Vector3 movement = source.transform.position - previousPosition.Value;
+            Vector3? direction = directionSource?.Interface?.Vector;
+            bool didMoveAlongDirection = direction != null
+                && Mathf.Abs(Vector3.Dot(direction.Value.normalized, movement.normalized))
+                >= directionalSimilarityThreshold;
+            if (didMoveAlongDirection == IsMovingAlongDirection)
+            {
+                return false;
+            }
+
+            IsMovingAlongDirection = didMoveAlongDirection;
+            return true;
+        }
+
+        /// <summary>
+        /// Emits the respective event based on the current state of moving along the direction.
+        /// </summary>
+        protected virtual void EmitMovementAlongDirectionEvent()
+        {
+            if (IsMovingAlongDirection)
+            {
+                StartedMovingAlongDirection?.Invoke();
+            }
+            else
+            {
+                StoppedMovingAlongDirection?.Invoke();
+            }
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/Follow/DirectionalMovementTrackerTest.cs
@@ -0,0 +1,130 @@
+﻿using VRTK.Core.Data.Source;
+using VRTK.Core.Tracking.Follow;
+
+namespace Test.VRTK.Core.Tracking.Follow
+{
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+    using UnityEngine;
+
+    public class DirectionalMovementTrackerTest
+    {
+        private DirectionalMovementTracker subject;
+        private GameObject source;
+        private UnityEventListenerMock startedMovingAlongDirectionListenerMock;
+        private UnityEventListenerMock stoppedMovingAlongDirectionListenerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new GameObject().AddComponent<DirectionalMovementTracker>();
+
+            source = new GameObject();
+            subject.source = source;
+
+            startedMovingAlongDirectionListenerMock = new UnityEventListenerMock();
+            subject.StartedMovingAlongDirection.AddListener(startedMovingAlongDirectionListenerMock.Listen);
+
+            stoppedMovingAlongDirectionListenerMock = new UnityEventListenerMock();
+            subject.StoppedMovingAlongDirection.AddListener(stoppedMovingAlongDirectionListenerMock.Listen);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject.gameObject);
+            Object.DestroyImmediate(source);
+        }
+
+        [Test]
+        public void EmitsNothingOnFirstProcess()
+        {
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+
+            subject.Process();
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+        }
+
+        [Test]
+        public void EmitsNothingWhenStayingStill()
+        {
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+
+            subject.Process();
+            subject.Process();
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+        }
+
+        [Test]
+        public void EmitsNothingWhenMovingInWrongDirection()
+        {
+            ConstantVector3Source directionSource = subject.gameObject.AddComponent<ConstantVector3Source>();
+            directionSource.vector = new Vector3(0f, 1f, 0f);
+            subject.directionSource = new Vector3SourceContainer
+            {
+                Interface = directionSource
+            };
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+
+            subject.Process();
+            source.transform.position += new Vector3(1f, 0f, 0f);
+            subject.Process();
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+        }
+
+        [Test]
+        public void EmitsStartedWhenMovingAlongDirection()
+        {
+            ConstantVector3Source directionSource = subject.gameObject.AddComponent<ConstantVector3Source>();
+            directionSource.vector = new Vector3(0f, 1f, 0f);
+            subject.directionSource = new Vector3SourceContainer
+            {
+                Interface = directionSource
+            };
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+
+            subject.Process();
+            source.transform.position += new Vector3(1f, 2f, 3f);
+            subject.Process();
+
+            Assert.IsTrue(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+        }
+
+        [Test]
+        public void EmitsStoppedWhenNoLongerMovingAlongDirection()
+        {
+            ConstantVector3Source directionSource = subject.gameObject.AddComponent<ConstantVector3Source>();
+            directionSource.vector = new Vector3(0f, 1f, 0f);
+            subject.directionSource = new Vector3SourceContainer
+            {
+                Interface = directionSource
+            };
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsFalse(stoppedMovingAlongDirectionListenerMock.Received);
+
+            subject.Process();
+            source.transform.position += new Vector3(-1f, -2f, -3f);
+            subject.Process();
+
+            startedMovingAlongDirectionListenerMock.Reset();
+            subject.Process();
+
+            Assert.IsFalse(startedMovingAlongDirectionListenerMock.Received);
+            Assert.IsTrue(stoppedMovingAlongDirectionListenerMock.Received);
+        }
+    }
+}
