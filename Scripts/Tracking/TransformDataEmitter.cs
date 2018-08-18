23f9bbbad1a1f456c36ae40b235a5bce51ea0048
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-08-18 12:44:00 +0200

    feat(Tracking): allow emitting TransformData from GameObject
    `TransformData` is used throughout the codebase to offer a more
    powerful representation of a `Transform`. The need to create an
    instance of the data type from a given Game Object arises in
    multiple places so a new component has been added to allow doing
    that without having to resort to do so in the script that needs it.
    The new component allows setting the source Game Object to create
    the `TransformData` for as well as emitting the event with that data
    manually.

---
+++ Scripts/Tracking/TransformDataEmitter.cs
@@ -0,0 +1,49 @@
+﻿namespace VRTK.Core.Tracking
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using VRTK.Core.Data.Type;
+
+    /// <summary>
+    /// Allows emitting an event with <see cref="TransformData"/> for a <see cref="GameObject"/>.
+    /// </summary>
+    public class TransformDataEmitter : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="TransformData"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<TransformData>
+        {
+        }
+
+        /// <summary>
+        /// The source to create the <see cref="TransformData"/> for.
+        /// </summary>
+        [Tooltip("The source to create the transform data for.")]
+        public GameObject source;
+
+        /// <summary>
+        /// Emitted whenever told to do so.
+        /// </summary>
+        public UnityEvent Called = new UnityEvent();
+
+        /// <summary>
+        /// Sets <see cref="source"/>.
+        /// </summary>
+        /// <param name="source">The new source to use from now on.</param>
+        public void SetSource(GameObject source)
+        {
+            this.source = source;
+        }
+
+        /// <summary>
+        /// Emits the event with the <see cref="TransformData"/> for <see cref="source"/>.
+        /// </summary>
+        public void EmitEvent()
+        {
+            Called?.Invoke(new TransformData(source));
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/TransformDataEmitterTest.cs
@@ -0,0 +1,82 @@
+﻿using VRTK.Core.Data.Type;
+using VRTK.Core.Tracking;
+
+namespace Test.VRTK.Core.Tracking
+{
+    using NUnit.Framework;
+    using UnityEngine;
+
+    public class TransformDataEmitterTest
+    {
+        private TransformDataEmitter subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new GameObject().AddComponent<TransformDataEmitter>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject.gameObject);
+        }
+
+        [Test]
+        public void SetSourceValid()
+        {
+            GameObject source = new GameObject();
+
+            Assert.IsNull(subject.source);
+            subject.SetSource(source);
+            Assert.AreEqual(source, subject.source);
+
+            Object.DestroyImmediate(source);
+        }
+
+        [Test]
+        public void SetSourceInvalid()
+        {
+            Assert.IsNull(subject.source);
+            subject.SetSource(null);
+            Assert.IsNull(subject.source);
+        }
+
+        [Test]
+        public void CalledEmittedWithInvalidSource()
+        {
+            CalledEventListenerMock calledListenerMock = new CalledEventListenerMock();
+            subject.Called.AddListener(calledListenerMock.Listen);
+
+            Assert.IsNull(calledListenerMock.ReceivedData);
+            subject.EmitEvent();
+            Assert.IsNull(calledListenerMock.ReceivedData.transform);
+        }
+
+        [Test]
+        public void CalledEmittedWithValidSource()
+        {
+            CalledEventListenerMock calledListenerMock = new CalledEventListenerMock();
+            subject.Called.AddListener(calledListenerMock.Listen);
+
+            GameObject source = new GameObject();
+            subject.SetSource(source);
+
+            Assert.IsNull(calledListenerMock.ReceivedData);
+            subject.EmitEvent();
+            Assert.AreEqual(source.transform, calledListenerMock.ReceivedData.transform);
+
+            Object.DestroyImmediate(source);
+        }
+
+        private sealed class CalledEventListenerMock
+        {
+            public TransformData ReceivedData;
+
+            public void Listen(TransformData data)
+            {
+                ReceivedData = data;
+            }
+        }
+    }
+}
