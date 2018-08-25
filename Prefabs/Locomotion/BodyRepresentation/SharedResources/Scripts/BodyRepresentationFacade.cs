359d048008adb930542fdada150be2416174a843
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-08-25 13:43:13 +0200

    feat(BodyRepresentation): add prefab for body representation
    A new prefab has been added that encapsulates and sets up a body
    representation. It additionally handles any necessary configuration
    of an optionally provided teleporter to utilize when the body walks
    up a slope or falls down to move the play area so the headset
    follows the virtual body in that scenario.

---
+++ Prefabs/Locomotion/BodyRepresentation/SharedResources/Scripts/BodyRepresentationFacade.cs
@@ -0,0 +1,27 @@
+﻿namespace VRTK.Core.Prefabs.Locomotion.BodyRepresentation
+{
+    using UnityEngine;
+    using VRTK.Core.Prefabs.Locomotion.Teleporters;
+
+    /// <summary>
+    /// The public interface for the BodyRepresentation prefab.
+    /// </summary>
+    public class BodyRepresentationFacade : MonoBehaviour
+    {
+        /// <summary>
+        /// The alias for the CameraRig Play Area.
+        /// </summary>
+        [Tooltip("The alias for the CameraRig Play Area.")]
+        public GameObject playAreaAlias;
+        /// <summary>
+        /// The alias for the CameraRig Headset.
+        /// </summary>
+        [Tooltip("The alias for the CameraRig Headset.")]
+        public GameObject headsetAlias;
+        /// <summary>
+        /// An optional teleporter to use.
+        /// </summary>
+        [Tooltip("An optional teleporter to use.")]
+        public TeleporterFacade teleporterFacade;
+    }
+}
---
+++ Prefabs/Locomotion/BodyRepresentation/SharedResources/Scripts/BodyRepresentationInternalSetup.cs
@@ -0,0 +1,85 @@
+﻿namespace VRTK.Core.Prefabs.Locomotion.BodyRepresentation
+{
+    using UnityEngine;
+    using VRTK.Core.Data.Type;
+    using VRTK.Core.Prefabs.Locomotion.Teleporters;
+    using VRTK.Core.Tracking;
+    using VRTK.Core.Tracking.CharacterController;
+    using VRTK.Core.Tracking.Modification;
+
+    /// <summary>
+    /// Sets up the BodyRepresentation Prefab based on the provided user settings.
+    /// </summary>
+    public class BodyRepresentationInternalSetup : MonoBehaviour
+    {
+        [Header("Facade Settings")]
+
+        /// <summary>
+        /// The public interface facade.
+        /// </summary>
+        [Tooltip("The public interface facade.")]
+        public BodyRepresentationFacade facade;
+
+        [Header("Alias Settings")]
+
+        /// <summary>
+        /// The <see cref="CharacterControllerHeightSetter"/> to set aliases on.
+        /// </summary>
+        [Tooltip("The Character Controller Height Setter to set aliases on.")]
+        public CharacterControllerHeightSetter heightSetter;
+        /// <summary>
+        /// The <see cref="CharacterControllerMover"/> to set aliases on.
+        /// </summary>
+        [Tooltip("The Character Controller Mover to set aliases on.")]
+        public CharacterControllerMover mover;
+        /// <summary>
+        /// The <see cref="TransformDataEmitter"/> to trigger teleportation for.
+        /// </summary>
+        [Tooltip("The Transform Data Emitter to trigger teleportation for.")]
+        public TransformDataEmitter transformDataEmitter;
+
+        protected virtual void OnEnable()
+        {
+            if (facade.teleporterFacade != null)
+            {
+                TeleporterInternalSetup teleporterSetup = facade.teleporterFacade.internalSetup;
+                TransformPropertyApplier transformPropertyApplier = teleporterSetup.surfaceTeleporter
+                    .gameObject.GetComponent<TransformPropertyApplier>();
+                if (transformPropertyApplier != null)
+                {
+                    transformPropertyApplier.applyOffsetOnAxis = Vector3State.True;
+                    teleporterSetup.modifyTeleporter = transformPropertyApplier;
+                }
+
+                foreach (SurfaceLocator surfaceLocator in teleporterSetup.surfaceLocatorAliases)
+                {
+                    surfaceLocator.enabled = false;
+                }
+            }
+
+            heightSetter.source = facade.headsetAlias;
+            heightSetter.offset = facade.playAreaAlias;
+
+            mover.target = facade.headsetAlias;
+
+            transformDataEmitter.Called.AddListener(Teleport);
+        }
+
+        protected virtual void OnDisable()
+        {
+            transformDataEmitter.Called.RemoveListener(Teleport);
+        }
+
+        /// <summary>
+        /// Triggers teleportation.
+        /// </summary>
+        /// <param name="data"></param>
+        protected virtual void Teleport(TransformData data)
+        {
+            if (facade.teleporterFacade != null)
+            {
+                facade.teleporterFacade.Teleport(data);
+            }
+        }
+    }
+}
